ca858e5c6fa4d903ee1802e0e3493c9f37f3f70d
PatriciaHorst <patricia_horst@estudante.sesisenai.org.br>
2026-09-18 15:45:48 -0300

    Documentação

--- src/main/java/br/com/mi81/dto/TarefaDto.java
+++ src/main/java/br/com/mi81/dto/TarefaDto.java
@@ -1,11 +1,16 @@
 package br.com.mi81.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record TarefaDto (
 
+        @Schema(description = "Titulo da tarefa")
     String titulo,
 
+        @Schema(description = "Descrição da tarefa")
     String descricao,
 
+        @Schema(description = "Prioridade da tarefa")
     String prioridade
 
 ){}
--- src/main/java/br/com/mi81/model/Tarefa.java
+++ src/main/java/br/com/mi81/model/Tarefa.java
@@ -1,6 +1,7 @@
 package br.com.mi81.model;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,13 +13,18 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Tarefa {
 
+    @Schema(description = "Id tarefa auto-increment ")
     private Integer id;
 
+    @Schema(description = "Titulo da tarefa")
     private String titulo;
 
+    @Schema(description = "Descrição da tarefa")
     private String descricao;
 
+    @Schema(description = "Prioridade de uma tarefa")
     private String prioridade;
 
+    @Schema(description = "Status da tarefa")
     private boolean status;
 }
