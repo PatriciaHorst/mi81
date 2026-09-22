320858f9bb4e05bc160893cf5a51c5bb2b04f170
Renan Daboit <renan_daboit@estudante.sesisenai.org.br>
2026-09-22 19:42:18 -0300

    Adiciona as validações nos atributos do TarefaRequestUpdate

--- src/main/java/br/com/mi81/dto/TarefaRequestUpdate.java
+++ src/main/java/br/com/mi81/dto/TarefaRequestUpdate.java
@@ -1,7 +1,19 @@
 package br.com.mi81.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
 public record TarefaRequestUpdate (
+
+        @NotNull
+        @Schema(description = "Titulo da tarefa")
         String titulo,
+
+        @NotNull
+        @Schema(description = "Descrição da tarefa")
         String descricao,
+
+        @NotNull
+        @Schema(description = "Prioridade da tarefa")
         String prioridade
 ){}
